910efec391d9b4feacb4018118b430552014158c
Anton Sviridov <asvirido@e1r6p4.unit.ua>
2018-03-05 19:41:23 +0200

    update lvl degree

--- main.py
+++ main.py
@@ -32,26 +32,10 @@
 #         return self.__pol
 #
 
-
 import sys
 
 def putstr(string):
     sys.stdout.write(string)
-
-class Polynomial:
-	a = 0.0
-	b = 0.0
-	c = 0.0
-	def 	__init__(self, argv):
-		self.inputParam = argv.split(" ")
-
-	def 	valid(self):
-		print self.inputParam
-		return -1
-
-	def 	convert(self):
-		res = self.valid()
-		return (res)
 
 def 	main_input():
 	putstr("Input: ")
@@ -61,6 +45,45 @@ def 	main_input():
 	except:
 		print "\nError input"
 		sys.exit(0)
+
+class Polynomial:
+	a = 0
+	b = 0
+	c = 0
+	countDigital = 0
+	countOperator = 0
+	countX = 0
+	lvlDegree = 0
+	def 	__init__(self, argv):
+		self.inputParam = argv.split(" ")
+
+	def 	valid(self):
+		print self.inputParam ### need delete
+		print "Len ", len(self.inputParam) ### need delete
+		for c in self.inputParam:
+			# print "test ", len(c)
+			size = len(c)
+			if ((c.isdigit()) == 1):
+				self.countDigital += 1
+			elif ((c.isalpha()) == 1):
+				print "Error: syntax error in ", c
+				sys.exit(1)
+			elif (('X^1' in c) or ('X^0' in c) or ('X^2' in c)):
+				self.countX += 1
+			elif (size == 3 and 'X^' in c and (c[2] != '0' or c[2] != '1' or c[2] != '2')):
+				print "Error: Level degree too big", c
+				sys.exit(1)
+			elif (size != 1):
+				print "Error: syntax error in ", c
+				sys.exit(1)
+
+		print "Count Digital", self.countDigital ## need delete
+		print "Count X ", self.countX ## need delete
+		return -1
+
+	def 	convert(self):
+		res = self.valid()
+		return (res)
 
 def main():
 	answer = main_input()
