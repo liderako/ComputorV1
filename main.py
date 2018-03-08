01327e0d25938b1c1437aead86dc713fc3e18c68
Anton Sviridov <asvirido@e1r6p5.unit.ua>
2018-03-08 15:39:52 +0200

    reduce and convert and findLvl done

--- main.py
+++ main.py
@@ -1,5 +1,56 @@
 import sys
+import numpy as np
 from module import *
+
+class   Matrix:
+    def     __init__(self, size):
+        self.matrixA = np.zeros(size)
+        self.matrixB = np.zeros(size)
+        self.matrixC = np.zeros(size)
+
+    def     convertMatrix(self, string):
+        old = '-1'
+        x = 0
+        A = 0
+        B = 0
+        C = 0
+        for c in string:
+            size = len(c)
+            if ((isfloat(c)) == True):
+                digital = float(c)
+                if (old == '-'):
+                    digital *= -1
+                tmpX = string[x + 2]
+                xLvl = tmpX[2]
+                old = 'd'
+                if xLvl == '0':
+                    self.matrixC[C] = digital
+                    C += 1
+                elif xLvl == '1':
+                    self.matrixB[B] = digital
+                    B += 1
+                elif xLvl == '2':
+                    self.matrixA[B] = digital
+                    B += 1
+            elif size == 1 and c[0] == '-':
+                old = '-'
+            x += 1
+    def     printMatrix(self, string): # need delete this function
+        print string
+        print "A",self.matrixA
+        print "B",self.matrixB
+        print "C",self.matrixC
+
+    def     mathMatrix(self, matrix, operator='+'):
+        x = 0.0
+        i = 0
+        while i < len(matrix):
+            if operator == '+':
+                x += matrix[i]
+            else:
+                x -= matrix[i]
+            i += 1
+        return x
 
 class Polynomial:
     a = 0
@@ -20,12 +71,15 @@ class Polynomial:
         string = self.join()
         partOne = self.parsString(string[0])
         partTwo = self.parsString(string[1])
-        self.reducePartOne(partOne)
-        
+        self.reduce(partOne, partTwo)
+        self.findLvlDegree()
+
         print "a ", self.a # need delete
         print "b ", self.b # need delete
         print "c ", self.c # need delete
-
+        print "Lvl degree", self.lvlDegree
+        sys.exit(1)
+    
     def     valid(self):
         self.CheckInputParam()
         self.checkRes()
@@ -38,7 +92,7 @@ class Polynomial:
                 if (old != '0' and old != 'o' and old != '='):
                     Error("Error: syntax error with before digital ", + c)
                 tmp  = float(c)
-                if (old == 'o' and tmp < 0):
+                if (tmp < 0):
                     Error("Error: math error with operator -")
                 self.countDigital += 1
                 old = 'd'
@@ -90,61 +144,27 @@ class Polynomial:
         res = res.split(" ")
         return res
 
-    def     reducePartOne(self, partOne):
-        size = len(partOne)
+    def     reduce(self, partOne, partTwo):
+        oneM = Matrix(self.countDigital)
+        twoM = Matrix(self.countDigital)
+        oneM.convertMatrix(partOne)
+        twoM.convertMatrix(partTwo)
+        # oneM.printMatrix("ONE") # need delete
+        # twoM.printMatrix("TWO") # need delete
+        self.c = oneM.mathMatrix(oneM.matrixC)
+        self.b = oneM.mathMatrix(oneM.matrixB)
+        self.a = oneM.mathMatrix(oneM.matrixA)
+        self.c = self.c + oneM.mathMatrix(twoM.matrixC, '-')
+        self.b = self.b + oneM.mathMatrix(twoM.matrixB, '-')
+        self.a = self.a + oneM.mathMatrix(twoM.matrixA, '-')
 
-    # def     reducePartOne(self, partOne):
-    #     size = len(partOne)
-        # digital = 0
-        # operator = '0'
-        # old = '0'
-        # i = 0
-        # for c in partOne:
-        #     c_len = len(c)
-        #     if ((isfloat(c)) == True):
-        #         digital = float(c)
-        #         tmp = partOne[i + 2]
-        #         lvl = tmp[2]
-        #         old = 'digital'
-        #         if ((operator == '0')):
-        #             self.doop(digital, lvl, '+')
-        #         else:
-        #             self.doop(digital, lvl, operator)
-        #     elif (c_len == 3 and (('X^1' in c) or ('X^0' in c) or ('X^2' in c))):
-        #         old = 'X'
-        #     elif c_len == 1:
-        #         if old == 'X':
-        #             operator = c[0]
-        #     i += 1
-    
-    # def     doop(self, digital, lvl, operator):
-    #     if (lvl == '0'):
-    #         if operator == '+':
-    #             self.c += digital
-    #         elif operator == '-':
-    #             self.c -= digital
-    #         elif operator == '*':
-    #             if (self.c == 0):
-    #                 self.c += 1
-    #             self.c *= digital
-    #     elif (lvl == '1'):
-    #         if operator == '+':
-    #             self.b += digital
-    #         elif operator == '-':
-    #             self.b -= digital
-    #         elif operator == '*':
-    #             if (self.b == 0):
-    #                 self.b += 1
-    #             self.b *= digital
-    #     else:
-    #         if operator == '+':
-    #             self.a += digital
-    #         elif operator == '-':
-    #             self.a -= digital
-    #         elif operator == '*':
-    #             if (self.a == 0):
-    #                 self.a += 1
-    #             self.a *= digital
+    def     findLvlDegree(self):
+        if (self.a != 0):
+            self.lvlDegree = 2
+        elif self.b != 0:
+            self.lvlDegree = 1
+        else:
+            self.lvlDegree = 0
 
 def main(argc, argv):
     if (argc != 2):
